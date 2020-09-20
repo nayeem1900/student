4a037b439074c2680b938176ff86dcce218004bf
nayeem1900 <43191178+nayeem1900@users.noreply.github.com>
2020-09-20 07:45:23 +0600

    student attendence css complete-1

--- resources/views/backend/employee/employee_attend/add-attendence.blade.php
+++ resources/views/backend/employee/employee_attend/add-attendence.blade.php
@@ -2,30 +2,36 @@
 
 @section('content')
 
-   {{-- <link rel="stylesheet"type="text/css" href="{{asset('public/backend/css/attend.css')}}">--}}
-    <style type="text/css">
-        .switch-toggle{
-            width: auto;
-        }
-        .switch-toggle label:not(.disabled){
-            cursor: pointer;
-        }
-        .switch-candy a{
-            border: 1px solid #333;
-            border-radius: 3px;
-            box-shadow: 0 1px 1px rgba(0,0,0,0.2),inset 0 1px 1px rgba(255,255,255,0.45);
-            background-color: white;
-            background-image: -webkit-linear-gradient(top,rgba(255,255,255,0.2),transparent);
-            background-image: linear-gradient(to bottom,rgba(255,255,255,0.2),transparent);
-        }
-        .switch-toggle .switch-candy,.switch-light.switch-candy >span{
-            background-color: #5a6268;
-            border-radius: 3px;
-            box-shadow: inset 0 2px 6px rgba(0,0,0,0.3),0 1px 0 rgba(255,255,255,0.2);
-        }
-    </style>
+   <link rel="stylesheet"type="text/css" href="{{asset('backend/css/attend.css')}}">
 
-    <div class="content-wrapper">
+   <style type="text/css">
+       .switch-toggle{
+           width: auto;
+       }
+       .switch-toggle label:not(.disabled){
+           cursor: pointer;
+       }
+       .switch-candy a{
+           border: 1px solid #333;
+           border-radius: 3px;
+           box-shadow: 0 1px 1px rgba(0,0,0,0.2),inset 0 1px 1px rgba(255,255,255,0.45);
+           background-color: white;
+           background-image: -webkit-linear-gradient(top,rgba(255,255,255,0.2),transparent);
+           background-image: linear-gradient(to bottom,rgba(255,255,255,0.2),transparent);
+       }
+       .switch-toggle .switch-candy,.switch-light.switch-candy >span{
+           background-color: #5a6268;
+           border-radius: 3px;
+           box-shadow: inset 0 2px 6px rgba(0,0,0,0.3),0 1px 0 rgba(255,255,255,0.2);
+       }
+   </style>
+
+
+
+
+
+
+   <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <div class="content-header">
             <div class="container-fluid">
